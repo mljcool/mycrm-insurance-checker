040b4939e634615328678344f37a74b0f2ef3870
MarkGocela <markloujames.gocela@devpartners.co>
2021-05-06 23:44:56 +0800

    add all assets for insurer and add card styles

--- scripts/extension/app.js
+++ scripts/extension/app.js
@@ -15,8 +15,53 @@ app.controller('appCtrl', [
   '$http',
   function($scope, $http) {
     $scope.clientList = [
-      { id: 1, fname: 'Frank', lname: 'Beans', prefix: 'FB' },
-      { id: 2, fname: 'Ryan', lname: 'Sandoval', prefix: 'RS' },
+      {
+        id: 1,
+        fname: 'Frank',
+        lname: 'Beans',
+        prefix: 'FB',
+
+        covers: [
+          {
+            id: 1,
+            name: 'Life Cover',
+            amount: 50000,
+          },
+          {
+            id: 2,
+            name: 'Trauma Cover',
+            amount: 5000,
+          },
+          {
+            id: 3,
+            name: 'Income Protection ',
+            amount: 11003,
+          },
+        ],
+      },
+      {
+        id: 2,
+        fname: 'Ryan',
+        lname: 'Sandoval',
+        prefix: 'RS',
+        covers: [
+          {
+            id: 1,
+            name: 'Life Cover',
+            amount: 213132,
+          },
+          {
+            id: 2,
+            name: 'Trauma Cover',
+            amount: 5000,
+          },
+          {
+            id: 3,
+            name: 'Income Protection ',
+            amount: 11003,
+          },
+        ],
+      },
     ];
   },
 ]);
